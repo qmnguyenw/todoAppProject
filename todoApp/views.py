345bccc916d7899bb04e2d840164681f9bac1e6f
Quang Nguyen <qmnguyenw@gmail.com>
2020-01-16 02:44:08 -0500

    create View (ViewSet)

--- todoApp/views.py
+++ todoApp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import TaskSerializer
+from .models import Task
 
-# Create your views here.
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all().order_by('-date_created')
+    serializer_class = TaskSerializer
