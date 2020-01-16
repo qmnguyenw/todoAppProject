6cb74c00675120201d11fa5212d8d5b560fc48bf
Quang Nguyen <qmnguyenw@gmail.com>
2020-01-16 03:34:45 -0500

    Modify Serializer for Image field & File Field

--- todoApp/models.py
+++ todoApp/models.py
@@ -3,8 +3,9 @@ from django.db import models
 class Task(models.Model):
     task_name = models.CharField(max_length=100)
     task_desc = models.TextField(max_length=200)
-    completed = models.BooleanField(default=False)
     date_created = models.DateTimeField(auto_now=True)
+    completed = models.BooleanField(default=False)
+    image = models.ImageField(upload_to='images/', default='images/none/none.jpg')
 
     def __str__(self):
         return "%s" % self.task_name
