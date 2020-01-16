d46a9ac3196b48db1e4e7aff4d48a55341772afe
Quang Nguyen <qmnguyenw@gmail.com>
2020-01-16 03:59:13 -0500

    Create a Folder in Base Project Directory to store MEDIA Data + Add MEDIA Related Settings (MEDIA_ROOT & _MEDIA_URL) in Settings.py file + Modify url.py in order to handle static media content

--- todoApp/serializers.py
+++ todoApp/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Task
 
 class TaskSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField(max_length=None, use_url=True)
     class Meta:
         model = Task
-        fields = ('id', 'task_name', 'task_desc', 'completed', 'date_created')
+        fields = ('id', 'task_name', 'task_desc', 'completed', 'date_created', 'image')
--- todoAppProject/urls.py
+++ todoAppProject/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
+from django.conf.urls.static import static
+from django.conf import settings
 from rest_framework import routers
 from todoApp import views
 
@@ -28,4 +30,4 @@ router.register(r'completed_task', views.CompletedTaskViewSet)
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^admin/', admin.site.urls),    
-]
+]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
