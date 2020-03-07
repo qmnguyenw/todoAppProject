a688dc5881254e6dc595959f9ad8427e3e2bc8b9
Quang Nguyen <qmnguyenw@gmail.com>
2020-03-07 10:18:43 +0700

    create authentication

--- todoApp/serializers.py
+++ todoApp/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Task
+from django.contrib.auth import get_user_model
 
 class TaskSerializer(serializers.ModelSerializer):
     image = serializers.ImageField(max_length=None, use_url=True)
@@ -7,3 +8,21 @@ class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = ('id', 'task_name', 'task_desc', 'completed', 'date_created', 'image', 'doc')
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    # override Create method
+    def create(self, validated_data):
+        user = get_user_model().objects.create(
+            username = validated_data['username']
+        )
+        # we assume there is custome auth model or build-in auth model
+        # custom auth model can we set by AUTH_USER_MODEL in settings.py
+        user.set_password(validated_data['password']) # we dont want to serialize password because it has been hashed
+        user.save()
+        return user
+
+    class Meta:
+        model = get_user_model()
+        fields = ('username', 'password')
--- todoApp/views.py
+++ todoApp/views.py
@@ -2,16 +2,20 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework import filters
 import django_filters.rest_framework
-from .serializers import TaskSerializer
+from .serializers import TaskSerializer, UserSerializer
 from .models import Task
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.generics import CreateAPIView
+from django.contrib.auth import get_user_model
 
 class TaskViewSet(viewsets.ModelViewSet):
-    queryset = Task.objects.all().order_by('-date_created')
+    permission_classes = (IsAuthenticated,) # only Authenticated User can perform CRUD operation
+    queryset = Task.objects.all() # use filter and ordering so remove order_by
     serializer_class = TaskSerializer
     filter_backends = (django_filters.rest_framework.DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter)
     filter_fields = ('completed',) # category task by attribute `completed`
     ordering = ('date_created',) # sorting by date_create
-    search_fields = ('task_name', 'task_desc',) # search for each column of db
+    search_fields = ('task_name', 'task_desc',) # search for each column of db, in here we can add '^', '=', '@', '$'
 
 # class DueTaskViewSet(viewsets.ModelViewSet):
 #     queryset = Task.objects.all().order_by('-date_created').filter(completed=False)
@@ -20,3 +24,10 @@ class TaskViewSet(viewsets.ModelViewSet):
 # class CompletedTaskViewSet(viewsets.ModelViewSet):
 #     queryset = Task.objects.all().order_by('-date_created').filter(completed=True)
 #     serializer_class = TaskSerializer
+
+class CreateUserView(CreateAPIView):
+    # createApiView provide only POST method
+    model = get_user_model() 
+    # set permission as allowany to register
+    permission_classes = (AllowAny,)
+    serializer_class = UserSerializer
