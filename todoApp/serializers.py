318b8ea3cc30f49412dfc4af2f7f78c08dd6503e
Quang Nguyen <qmnguyenw@gmail.com>
2020-03-07 10:19:49 +0700

    clear code

--- todoApp/serializers.py
+++ todoApp/serializers.py
@@ -3,8 +3,8 @@ from .models import Task
 from django.contrib.auth import get_user_model
 
 class TaskSerializer(serializers.ModelSerializer):
-    image = serializers.ImageField(max_length=None, use_url=True)
-    doc = serializers.FileField(max_length=None, use_url=True)
+    image = serializers.ImageField(max_length=None, use_url=True, default='images/none/none.jpg')
+    doc = serializers.FileField(max_length=None, use_url=True, default='docs/none/none.txt')
     class Meta:
         model = Task
         fields = ('id', 'task_name', 'task_desc', 'completed', 'date_created', 'image', 'doc')
